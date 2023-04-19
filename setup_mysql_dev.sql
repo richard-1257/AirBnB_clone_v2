eecf6985aa96fa95fc7e21e3bbc64af134f45f34
richard <richardsifeanyi@gmail.com>
2023-04-19 04:52:10 -0700

    MySQL setup development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,5 @@
+-- Prepare a MYSQL server for the project.
+CREATE DATABASE IF NOT EXITS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED By 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
