8f92c8bba8360c98a813b851aa3b56b70369d064
richard <richardsifeanyi@gmail.com>
2023-04-20 05:06:57 -0700

    changes

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -21,4 +21,4 @@ class test_City(test_basemodel):
     def test_name(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(type(new.name), )
