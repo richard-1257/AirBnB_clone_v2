66483448b058d4d5eba5541817608d693f09302f
richard <richardsifeanyi@gmail.com>
2023-04-20 04:47:33 -0700

    added appropriate unittests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,34 +1,24 @@
 #!/usr/bin/python3
 """ """
 from tests.test_models.test_base_model import test_basemodel
-from models.user import User
+from models.city import City
 
 
-class test_User(test_basemodel):
+class test_City(test_basemodel):
     """ """
 
     def __init__(self, *args, **kwargs):
         """ """
         super().__init__(*args, **kwargs)
-        self.name = "User"
-        self.value = User
+        self.name = "City"
+        self.value = City
 
-    def test_first_name(self):
+    def test_state_id(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.first_name), str)
+        self.assertEqual(type(new.state_id), str)
 
-    def test_last_name(self):
+    def test_name(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.last_name), str)
-
-    def test_email(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.email), str)
-
-    def test_password(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.password), str)
+        self.assertEqual(type(new.name), str)
